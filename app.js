75be9270240640481b09fca61b6e6259fdee6a02
DeliaMtr <deliamutiara@student.upi.edu>
2026-09-22 23:35:54 +0700

    mengerjakan aktivitas ronde ketiga

--- app.js
+++ app.js
@@ -235,25 +235,43 @@ console.log("Nama       : " + pelangganC);
 console.log("Total Poin : " + totalPoinC);
 console.log("Tier       : " + tierPelangganC);
 
-
 // ============================================================
 // AKTIVITAS 6: Array & For Loop — Daftar Menu Rekomendasi
 // ============================================================
 
 // TODO 6A:
 // Buat variabel Array bernama "menuRekomendasi" yang berisi minimal 5 nama menu kopi/makanan.
-
-
-
+let menuRekomendasi = [
+    "Kopi Susu Gula Aren",
+    "Cappuccino",
+    "Matcha Latte",
+    "Croissant",
+    "Chocolate Cake"
+];
+console.log("");
+console.log("=== MENU REKOMENDASI KEDAI ===");
 
 // TODO 6B:
 // Gunakan perulangan "for loop" untuk mencetak setiap menu ke Console dengan format:
 // "1. Nama Menu", "2. Nama Menu", dst. Gunakan (i + 1) untuk nomor urutnya.
-
-
-
+for (
+    let i = 0;
+    i < menuRekomendasi.length;
+    i++
+) {
+    console.log(
+        (i + 1) +
+        ". " +
+        menuRekomendasi[i]
+    );
+}
 
 // TODO 6C:
 // Cetak jumlah total menu di akhir daftar menggunakan properti ".length".
 // Akhiri program dengan: console.log("=== TUGAS MANDIRI SELESAI DENGAN SUKSES! ===");
-
+console.log(
+    "Jumlah Menu Rekomendasi : " +
+    menuRekomendasi.length
+);
+console.log("");
+console.log("=== PROGRAM SELESAI ===");
